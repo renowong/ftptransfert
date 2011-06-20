9a88b908ead137cdf2326722650c232dfd543b01
rwong <rwong@de1159cc-c40b-e1ae-674e-5a5ac6ea50b3>
2011-06-20 20:44:43 +0000

    db and upload working

---
+++ global_vars.php
@@ -0,0 +1,6 @@
+<?php
+define("HOST","192.168.0.8");
+define("DB","ftptransfert");
+define("DBUSER","ftp");
+define("DBPASSWORD","ftp");
+?>
---
+++ index.php
@@ -0,0 +1,86 @@
+<?
+include("global_vars.php");
+if(!isset($_POST["submit"])){?>
+ 
+<form action="index.php" method="POST" enctype="multipart/form-data">
+<table align="center">
+<tr>
+<td align="right">
+Selectionner votre fichier:
+</td>
+<td>
+<input name="userfile" type="file" size="50">
+</td>
+</tr>
+</table>
+<table align="center">
+<tr>
+<td align="center">
+<input type="submit" name="submit" value="Transferer" />
+</td>
+</tr>
+ 
+</table>
+</form>
+<?}
+else 
+{
+ 
+set_time_limit(300);//for setting 
+ 
+$paths="/";
+ 
+$filep=$_FILES['userfile']['tmp_name'];
+ 
+$ftp_server="192.168.0.8";
+ 
+$ftp_user_name="ftp";
+ 
+$ftp_user_pass="ftp";
+ 
+$truename=$_FILES['userfile']['name'];
+ 
+$name=md5($truename);
+
+
+updatedb($name,$truename);
+ 
+// set up a connection to ftp server
+$conn_id = ftp_connect($ftp_server);
+ 
+// login with username and password
+$login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
+ 
+// check connection and login result
+if ((!$conn_id) || (!$login_result)) {
+       echo "FTP connection has encountered an error!";
+       echo "Attempted to connect to $ftp_server for user $ftp_user_name....";
+       exit;
+   } else {
+       echo "Connected to $ftp_server, for user $ftp_user_name".".....";
+   }
+ 
+// upload the file to the path specified
+$upload = ftp_put($conn_id, $paths.'/'.$name, $filep, FTP_BINARY);
+ 
+// check the upload status
+if (!$upload) {
+       echo "FTP upload has encountered an error!";
+   } else {
+       echo "Uploaded file with name $name to $ftp_server ";
+   }
+ 
+// close the FTP connection
+ftp_close($conn_id);	
+ 
+ 
+
+}
+
+function updatedb($md5,$name){
+    $mysqli = new mysqli(HOST, DBUSER, DBPASSWORD, DB);
+    $query = "INSERT INTO `ftptransfert`.`files` (`ID` ,`md5` ,`name`) VALUES (NULL ,  '$md5',  '$name')";
+    $mysqli->query($query);
+    $mysqli->close();
+}
+?>
